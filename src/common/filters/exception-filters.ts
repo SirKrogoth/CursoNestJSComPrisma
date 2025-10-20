2196cdaf548fc5478a54e626fcaca98e8541c64a
João Rafael Fagundes Menezes <menezes.jrafael@gmail.com>
2025-10-19 23:31:07 -0300

    Foi umas coisas ai

---
+++ src/common/filters/exception-filters.ts
@@ -0,0 +1,23 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+import { error, time } from "console";
+import { errorMonitor } from "events";
+
+@Catch(HttpException)
+export class ApiExceptionFilter implements ExceptionFilter {
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse();
+        const request = ctx.getRequest();
+        const status = exception.getStatus();
+        const exceptionResponse = exception.getResponse();
+
+        console.log("PASSANDO PELO FILTER");
+
+        response.status(status).json({
+            statusCode: status,
+            timestamp: new Date().toISOString(),
+            message: exceptionResponse !== "" ? exceptionResponse : "Erro ao realizar esta operação",
+            path: request.url
+        });
+    }
+}
